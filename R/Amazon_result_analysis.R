c8e07bf717409033f2541666692d8e908a12c1ec
mingkaijiang <shanyujordan512@gmail.com>
2017-06-27 16:46:48 +1000

    added amazon script and updated to do list

---
+++ R/Amazon_result_analysis.R
@@ -0,0 +1,120 @@
+#### AmazonFACE CO2 response analysis
+#### This script is to accompany the UK workshop EucFACE plot
+#### To provide comparison of AmazonFACE CO2 response
+#### Although we are not having the same CO2 treatment
+
+### Read in files
+## DE relationship, N only
+de_n_DF2 <- read.csv("~/Documents/Research/Projects/Amazon/AMAZ/drought/outputs/AmaFACE1_D_GDA_AMB_OBS.csv", skip=3)
+de_n_DF3 <- read.csv("~/Documents/Research/Projects/Amazon/AMAZ/drought/outputs/AmaFACE1_D_GDA_ELE_OBS.csv", skip=3)
+
+## DE relationship, N P 
+de_np_DF2 <- read.csv("~/Documents/Research/Projects/Amazon/AMAZ/drought_p/outputs/AmaFACE1_D_GDP_AMB_OBS.csv", skip=3)
+de_np_DF3 <- read.csv("~/Documents/Research/Projects/Amazon/AMAZ/drought_p/outputs/AmaFACE1_D_GDP_ELE_OBS.csv", skip=3)
+
+
+#### Process the data to plot annual patterns
+## Generate output DF at annual timestep
+gppDF <- data.frame(seq(2012, 2023, by=1), NA, NA, NA, NA)
+colnames(gppDF) <- c("Year", "gday_n_amb","gday_p_amb", "gday_n_ele", "gday_p_ele")
+laiDF <- gppDF
+leafDF <- gppDF
+npDF <- gppDF
+woodDF <- gppDF
+
+## store data
+for (i in 2012:2023) {
+    # GPP
+    gppDF[gppDF$Year == i, "gday_n_amb"] <- sum(de_n_DF2[de_n_DF2$YEAR == i, "GPP"], na.rm=T)
+    gppDF[gppDF$Year == i, "gday_p_amb"] <- sum(de_np_DF2[de_np_DF2$YEAR == i, "GPP"], na.rm=T)
+    gppDF[gppDF$Year == i, "gday_n_ele"] <- sum(de_n_DF3[de_n_DF3$YEAR == i, "GPP"], na.rm=T)
+    gppDF[gppDF$Year == i, "gday_p_ele"] <- sum(de_np_DF3[de_np_DF3$YEAR == i, "GPP"], na.rm=T)
+
+    # LAI
+    laiDF[laiDF$Year == i, "gday_n_amb"] <- de_n_DF2[de_n_DF2$YEAR == i & de_n_DF2$DOY == 1, "LAI"]
+    laiDF[laiDF$Year == i, "gday_p_amb"] <- de_np_DF2[de_np_DF2$YEAR == i & de_np_DF2$DOY == 1, "LAI"]
+    laiDF[laiDF$Year == i, "gday_n_ele"] <- de_n_DF3[de_n_DF3$YEAR == i & de_n_DF3$DOY == 1, "LAI"]
+    laiDF[laiDF$Year == i, "gday_p_ele"] <- de_np_DF3[de_np_DF3$YEAR == i & de_np_DF3$DOY == 1, "LAI"]
+    
+    # Leaf C
+    leafDF[leafDF$Year == i, "gday_n_amb"] <- de_n_DF2[de_n_DF2$YEAR == i & de_n_DF2$DOY == 1, "CL"]
+    leafDF[leafDF$Year == i, "gday_p_amb"] <- de_np_DF2[de_np_DF2$YEAR == i & de_np_DF2$DOY == 1, "CL"]
+    leafDF[leafDF$Year == i, "gday_n_ele"] <- de_n_DF3[de_n_DF3$YEAR == i & de_n_DF3$DOY == 1, "CL"]
+    leafDF[leafDF$Year == i, "gday_p_ele"] <- de_np_DF3[de_np_DF3$YEAR == i & de_np_DF3$DOY == 1, "CL"]
+    
+    
+    # Leaf N:P
+    npDF[npDF$Year == i, "gday_n_amb"] <- de_n_DF2[de_n_DF2$YEAR == i & de_n_DF2$DOY == 1, "NL"]/de_n_DF2[de_n_DF2$YEAR == i & de_n_DF2$DOY == 1, "PL"]
+    npDF[npDF$Year == i, "gday_p_amb"] <- de_np_DF2[de_np_DF2$YEAR == i & de_np_DF2$DOY == 1, "NL"]/de_np_DF2[de_np_DF2$YEAR == i & de_np_DF2$DOY == 1, "PL"]
+    npDF[npDF$Year == i, "gday_n_ele"] <- de_n_DF3[de_n_DF3$YEAR == i & de_n_DF3$DOY == 1, "NL"]/de_n_DF3[de_n_DF3$YEAR == i & de_n_DF3$DOY == 1, "PL"]
+    npDF[npDF$Year == i, "gday_p_ele"] <- de_np_DF3[de_np_DF3$YEAR == i & de_np_DF3$DOY == 1, "NL"]/de_np_DF3[de_np_DF3$YEAR == i & de_np_DF3$DOY == 1, "PL"]
+    
+}
+
+
+# Compute CO2 % response
+gppDF[,"gday_n"] <- (gppDF[,"gday_n_ele"] - gppDF[,"gday_n_amb"])/gppDF[,"gday_n_amb"] * 100.0
+laiDF[,"gday_n"] <- (laiDF[,"gday_n_ele"] - laiDF[,"gday_n_amb"])/laiDF[,"gday_n_amb"] * 100.0
+leafDF[,"gday_n"] <- (leafDF[,"gday_n_ele"] - leafDF[,"gday_n_amb"])/leafDF[,"gday_n_amb"] * 100.0
+npDF[,"gday_n"] <- (npDF[,"gday_n_ele"] - npDF[,"gday_n_amb"])/npDF[,"gday_n_amb"] * 100.0
+woodDF[,"gday_n"] <- (woodDF[,"gday_n_ele"] - woodDF[,"gday_n_amb"])/woodDF[,"gday_n_amb"] * 100.0
+
+
+gppDF[,"gday_p"] <- (gppDF[,"gday_p_ele"] - gppDF[,"gday_p_amb"])/gppDF[,"gday_p_amb"] * 100.0
+laiDF[,"gday_p"] <- (laiDF[,"gday_p_ele"] - laiDF[,"gday_p_amb"])/laiDF[,"gday_p_amb"] * 100.0
+leafDF[,"gday_p"] <- (leafDF[,"gday_p_ele"] - leafDF[,"gday_p_amb"])/leafDF[,"gday_p_amb"] * 100.0
+npDF[,"gday_p"] <- (npDF[,"gday_p_ele"] - npDF[,"gday_p_amb"])/npDF[,"gday_p_amb"] * 100.0
+woodDF[,"gday_p"] <- (woodDF[,"gday_p_ele"] - woodDF[,"gday_p_amb"])/woodDF[,"gday_p_amb"] * 100.0
+
+
+
+pdf("R/UK_workshop_parameters_amazon.pdf", width = 9, height = 9)
+m <- matrix(c(1,2,3,4,5,5),nrow = 3, ncol = 2, byrow = TRUE)
+
+layout(mat = m,heights = c(0.4,0.4,0.2))
+
+par(mar=c(5.1, 6.1, 3.1, 6.1),
+    mgp=c(4,1,0))
+
+# GPP
+with(gppDF, plot(gday_n~Year, ylim=c(-10,30), 
+                 ylab = "GPP % response",cex.axis = 1.5,
+                 type="l", lwd = 3, col = "brown", cex.lab = 2.5))
+with(gppDF, points(gday_p~Year, type="l", col = "red", lwd = 3))
+
+# LAI
+with(laiDF, plot(gday_n~Year, ylim=c(-10,30), 
+                 ylab = "LAI % response",cex.axis = 1.5,
+                 type="l", lwd = 3, col = "brown", cex.lab = 2.5))
+with(laiDF, points(gday_p~Year, type="l", col = "red", lwd = 3))
+
+# Leaf C
+with(leafDF, plot(gday_n~Year, ylim=c(-10,30), 
+                  ylab = "Leaf C % response",cex.axis = 1.5,
+                  type="l", lwd = 3, col = "brown", cex.lab = 2.5))
+with(leafDF, points(gday_p~Year, type="l", col = "red", lwd = 3))
+
+# Leaf NP
+with(npDF, plot(gday_n~Year, ylim=c(-10,10), 
+                ylab = "Leaf N:P ratio % response",cex.axis = 1.5,
+                type="l", lwd = 3, col = "brown", cex.lab = 2.5))
+with(npDF, points(gday_p~Year, type="l", col = "red", lwd = 3))
+
+
+plot(1, type = "n", axes=FALSE, xlab="", ylab="")
+legend("top",
+       legend = c("N only", "NP model"), 
+       col=c("brown", "red"), lwd=5, cex=1.5, horiz = TRUE, pt.cex = 5,
+       pt.lwd = 5)
+
+dev.off()
+
+
+
+### Will need to subtract met data for 2012-2023 for AmazonFACE and run model just for these periods using CO2 data from EucFACE
+### So that we are consistently comparing the same CO2 effect
+
+### TO do list:
+### 1. run AmazonFACE
+### 2. run quasi-equil framework with parameters from EucFACE and AmazonFACE, plotting
+### 3. Plot inst, L and VL constraint comparisons.
--- R/GDAY_simulation_analysis_script.R
+++ R/GDAY_simulation_analysis_script.R
@@ -78,52 +78,109 @@ dev.off()
 
 #### Process the data to plot annual patterns
 ## Generate output DF at annual timestep
-gppDF <- data.frame(seq(2012, 2023, by=1), NA, NA)
-colnames(gppDF) <- c("Year", "gday_n","gday_p")
+gppDF <- data.frame(seq(2012, 2023, by=1), NA, NA, NA, NA)
+colnames(gppDF) <- c("Year", "gday_n_amb","gday_p_amb", "gday_n_ele", "gday_p_ele")
 laiDF <- gppDF
 leafDF <- gppDF
 npDF <- gppDF
+woodDF <- gppDF
 
 ## store data
 for (i in 2012:2023) {
     # GPP
-    gppDF[gppDF$Year == i, "gday_n"] <- sum(de_n_DF2[de_n_DF2$year == i, "gpp"], na.rm=T)
-    gppDF[gppDF$Year == i, "gday_p"] <- sum(de_np_DF2[de_np_DF2$year == i, "gpp"], na.rm=T)
+    gppDF[gppDF$Year == i, "gday_n_amb"] <- sum(de_n_DF2[de_n_DF2$year == i, "gpp"], na.rm=T)
+    gppDF[gppDF$Year == i, "gday_p_amb"] <- sum(de_np_DF2[de_np_DF2$year == i, "gpp"], na.rm=T)
+    gppDF[gppDF$Year == i, "gday_n_ele"] <- sum(de_n_DF3[de_n_DF3$year == i, "gpp"], na.rm=T)
+    gppDF[gppDF$Year == i, "gday_p_ele"] <- sum(de_np_DF3[de_np_DF3$year == i, "gpp"], na.rm=T)
     
     # LAI
-    laiDF[laiDF$Year == i, "gday_n"] <- de_n_DF2[de_n_DF2$year == i & de_n_DF2$doy == 1, "lai"]
-    laiDF[laiDF$Year == i, "gday_p"] <- de_np_DF2[de_np_DF2$year == i & de_np_DF2$doy == 1, "lai"]
+    laiDF[laiDF$Year == i, "gday_n_amb"] <- de_n_DF2[de_n_DF2$year == i & de_n_DF2$doy == 1, "lai"]
+    laiDF[laiDF$Year == i, "gday_p_amb"] <- de_np_DF2[de_np_DF2$year == i & de_np_DF2$doy == 1, "lai"]
+    laiDF[laiDF$Year == i, "gday_n_ele"] <- de_n_DF3[de_n_DF3$year == i & de_n_DF3$doy == 1, "lai"]
+    laiDF[laiDF$Year == i, "gday_p_ele"] <- de_np_DF3[de_np_DF3$year == i & de_np_DF3$doy == 1, "lai"]
     
     # Leaf C
-    leafDF[leafDF$Year == i, "gday_n"] <- de_n_DF2[de_n_DF2$year == i & de_n_DF2$doy == 1, "shoot"]
-    leafDF[leafDF$Year == i, "gday_p"] <- de_np_DF2[de_np_DF2$year == i & de_np_DF2$doy == 1, "shoot"]
+    leafDF[leafDF$Year == i, "gday_n_amb"] <- de_n_DF2[de_n_DF2$year == i & de_n_DF2$doy == 1, "shoot"]
+    leafDF[leafDF$Year == i, "gday_p_amb"] <- de_np_DF2[de_np_DF2$year == i & de_np_DF2$doy == 1, "shoot"]
+    leafDF[leafDF$Year == i, "gday_n_ele"] <- de_n_DF3[de_n_DF3$year == i & de_n_DF3$doy == 1, "shoot"]
+    leafDF[leafDF$Year == i, "gday_p_ele"] <- de_np_DF3[de_np_DF3$year == i & de_np_DF3$doy == 1, "shoot"]
+
     
     # Leaf N:P
-    npDF[npDF$Year == i, "gday_n"] <- de_n_DF2[de_n_DF2$year == i & de_n_DF2$doy == 1, "shootn"]/de_n_DF2[de_n_DF2$year == i & de_n_DF2$doy == 1, "shootp"]
-    npDF[npDF$Year == i, "gday_p"] <- de_np_DF2[de_np_DF2$year == i & de_np_DF2$doy == 1, "shootn"]/de_np_DF2[de_np_DF2$year == i & de_np_DF2$doy == 1, "shootp"]
+    npDF[npDF$Year == i, "gday_n_amb"] <- de_n_DF2[de_n_DF2$year == i & de_n_DF2$doy == 1, "shootn"]/de_n_DF2[de_n_DF2$year == i & de_n_DF2$doy == 1, "shootp"]
+    npDF[npDF$Year == i, "gday_p_amb"] <- de_np_DF2[de_np_DF2$year == i & de_np_DF2$doy == 1, "shootn"]/de_np_DF2[de_np_DF2$year == i & de_np_DF2$doy == 1, "shootp"]
+    npDF[npDF$Year == i, "gday_n_ele"] <- de_n_DF3[de_n_DF3$year == i & de_n_DF3$doy == 1, "shootn"]/de_n_DF3[de_n_DF3$year == i & de_n_DF3$doy == 1, "shootp"]
+    npDF[npDF$Year == i, "gday_p_ele"] <- de_np_DF3[de_np_DF3$year == i & de_np_DF3$doy == 1, "shootn"]/de_np_DF3[de_np_DF3$year == i & de_np_DF3$doy == 1, "shootp"]
     
+    # wood C
+    woodDF[woodDF$Year == i, "gday_n_amb"] <- de_n_DF2[de_n_DF2$year == i & de_n_DF2$doy == 1, "stem"]
+    woodDF[woodDF$Year == i, "gday_p_amb"] <- de_np_DF2[de_np_DF2$year == i & de_np_DF2$doy == 1, "stem"]
+    woodDF[woodDF$Year == i, "gday_n_ele"] <- de_n_DF3[de_n_DF3$year == i & de_n_DF3$doy == 1, "stem"]
+    woodDF[woodDF$Year == i, "gday_p_ele"] <- de_np_DF3[de_np_DF3$year == i & de_np_DF3$doy == 1, "stem"]
 }
 
+
+# Compute CO2 % response
+gppDF[,"gday_n"] <- (gppDF[,"gday_n_ele"] - gppDF[,"gday_n_amb"])/gppDF[,"gday_n_amb"] * 100.0
+laiDF[,"gday_n"] <- (laiDF[,"gday_n_ele"] - laiDF[,"gday_n_amb"])/laiDF[,"gday_n_amb"] * 100.0
+leafDF[,"gday_n"] <- (leafDF[,"gday_n_ele"] - leafDF[,"gday_n_amb"])/leafDF[,"gday_n_amb"] * 100.0
+npDF[,"gday_n"] <- (npDF[,"gday_n_ele"] - npDF[,"gday_n_amb"])/npDF[,"gday_n_amb"] * 100.0
+woodDF[,"gday_n"] <- (woodDF[,"gday_n_ele"] - woodDF[,"gday_n_amb"])/woodDF[,"gday_n_amb"] * 100.0
+
+
+gppDF[,"gday_p"] <- (gppDF[,"gday_p_ele"] - gppDF[,"gday_p_amb"])/gppDF[,"gday_p_amb"] * 100.0
+laiDF[,"gday_p"] <- (laiDF[,"gday_p_ele"] - laiDF[,"gday_p_amb"])/laiDF[,"gday_p_amb"] * 100.0
+leafDF[,"gday_p"] <- (leafDF[,"gday_p_ele"] - leafDF[,"gday_p_amb"])/leafDF[,"gday_p_amb"] * 100.0
+npDF[,"gday_p"] <- (npDF[,"gday_p_ele"] - npDF[,"gday_p_amb"])/npDF[,"gday_p_amb"] * 100.0
+woodDF[,"gday_p"] <- (woodDF[,"gday_p_ele"] - woodDF[,"gday_p_amb"])/woodDF[,"gday_p_amb"] * 100.0
+
+
+pdf("R/UK_workshop_parameters.pdf", width = 9, height = 9)
+m <- matrix(c(1,2,3,4,5,5),nrow = 3, ncol = 2, byrow = TRUE)
+
+layout(mat = m,heights = c(0.4,0.4,0.2))
+
+par(mar=c(5.1, 6.1, 3.1, 6.1),
+    mgp=c(4,1,0))
+
 # GPP
-with(gppDF, plot(gday_n*100~Year, ylim=c(0,3000), 
-                 ylab = expression(paste("GPP [g C ", m^-2, " ", yr^-1, "]")),
-                 type="l", lwd = 2, col = "brown"))
-with(gppDF, points(gday_p*100~Year, type="l", col = "red", lwd = 2))
+with(gppDF, plot(gday_n~Year, ylim=c(-10,30), 
+                 ylab = "GPP % response",cex.axis = 1.5,
+                 type="l", lwd = 3, col = "brown", cex.lab = 2.5))
+with(gppDF, points(gday_p~Year, type="l", col = "red", lwd = 3))
 
 # LAI
-with(laiDF, plot(gday_n~Year, ylim=c(1,2), 
-                 ylab = "LAI",
-                 type="l", lwd = 2, col = "brown"))
-with(laiDF, points(gday_p~Year, type="l", col = "red", lwd = 2))
+with(laiDF, plot(gday_n~Year, ylim=c(-10,30), 
+                 ylab = "LAI % response",cex.axis = 1.5,
+                 type="l", lwd = 3, col = "brown", cex.lab = 2.5))
+with(laiDF, points(gday_p~Year, type="l", col = "red", lwd = 3))
 
 # Leaf C
-with(leafDF, plot(gday_n*100~Year, ylim=c(0,300), 
-                 ylab = expression(paste("Leaf C [g C ", m^-2, " ", yr^-1, "]")),
-                 type="l", lwd = 2, col = "brown"))
-with(leafDF, points(gday_p*100~Year, type="l", col = "red", lwd = 2))
+with(leafDF, plot(gday_n~Year, ylim=c(-10,30), 
+                 ylab = "Leaf C % response",cex.axis = 1.5,
+                 type="l", lwd = 3, col = "brown", cex.lab = 2.5))
+with(leafDF, points(gday_p~Year, type="l", col = "red", lwd = 3))
 
 # Leaf NP
-with(npDF, plot(gday_n~Year, ylim=c(0,100), 
-                  ylab = "Leaf N:P ratio",
+with(npDF, plot(gday_n~Year, ylim=c(-10,10), 
+                  ylab = "Leaf N:P ratio % response",cex.axis = 1.5,
+                  type="l", lwd = 3, col = "brown", cex.lab = 2.5))
+with(npDF, points(gday_p~Year, type="l", col = "red", lwd = 3))
+
+
+plot(1, type = "n", axes=FALSE, xlab="", ylab="")
+legend("top",
+       legend = c("N only", "NP model"), 
+       col=c("brown", "red"), lwd=5, cex=1.5, horiz = TRUE, pt.cex = 5,
+       pt.lwd = 5)
+
+dev.off()
+
+
+
+# Wood C
+with(woodDF, plot(gday_n~Year, ylim=c(-10,30), 
+                  ylab = "Wood C % response",
                   type="l", lwd = 2, col = "brown"))
-with(npDF, points(gday_p~Year, type="l", col = "red", lwd = 2))
+with(leafDF, points(gday_p~Year, type="l", col = "red", lwd = 2))
+
